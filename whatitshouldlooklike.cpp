f4295a3e717e2e457f69d472f9f8dce97aa65ad8
Guichard Manigat <128933880+gukardma@users.noreply.github.com>
2024-02-27 19:51:16 -0500

    Update whatitshouldlooklike.cpp

--- whatitshouldlooklike.cpp
+++ whatitshouldlooklike.cpp
@@ -666,7 +666,6 @@ vector<slide_puzzle_t> slpz_search_itdp_r(
         vector<slide_puzzle_t> &path,
         int depth) {
     vector<slide_puzzle_t> seq;
-    // TODO: Condition for checking that path to solution was found
     if (depth == 0) {
         return seq;
     }
@@ -690,45 +689,7 @@ vector<slide_puzzle_t> slpz_search_itdp_r(
     }
     return seq;
 }
-//    if(start_sp == end_sp) {
-//        // Found a puzzle solution path that leads to the goal, at the desired
-//        // path length
-//        seq = path;
-//        // assert(depth == 0); // should be at zero depth by now
-//    // TODO: Condition for checking that more paths should be considered
-//    } else if(false) {
-//        // Have not yet found a solution, but still have deeper paths remaining
-//        // to explore
-//        const slide_puzzle_t& curr_sp(start_sp);
-//
-//        // List of possible next moves
-//        /* const */ vector<slide_puzzle_t> next_possible_sp;
-//
-//        // To avoid cycles, do not go back to puzzles that have already
-//        // been visited
-//        for (const auto &next_sp: next_possible_sp) {
-//            // TODO: fix condition
-//            if (false) {
-//                // This puzzle has already been visited,
-//                // so do nothing.
-//            } else {
-//                // This puzzle has not been visited, so visit it.
-//                // TODO: Adjust visited_sp and path
-//                vector<slide_puzzle_t> subseq;
-//                // TODO: recursion
-//                //subseq = slpz_search_itdp_r(...,
-//                //        ..., visited_sp, ..., depth - 1);
-//                if(!subseq.empty()) {
-//                    // Found a solution
-//                    seq = subseq;
-//                    break; // prefer a different approach later...
-//                }
-//                // TODO: Readjust visited_sp and path
-//            }
-//        }
-//    }
-//    return seq;
-//}
+
 
 // ITEM: Implement slide puzzle solution using iterative deepening (entry)
 /**
